0bb9eb94f1e2b61770e91f63af85939d9c8ecbaa
RaghavNagi <raghavnagi81@gmail.com>
2026-04-01 23:44:26 +0530

    hierarchy setup for frontend and login and register ui created

---
+++ frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <main>
+        <div className='form-container'>
+            <h1>Login</h1>
+
+            <form>
+
+            <div className="input-group"></div>
+
+            </form>
+        </div>
+    </main>
+  )
+}
