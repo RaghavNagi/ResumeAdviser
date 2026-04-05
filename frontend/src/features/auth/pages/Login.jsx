7c69d8c3798a0a19658ce970ab6a8854cc8fede8
RaghavNagi <raghavnagi81@gmail.com>
2026-04-06 01:27:29 +0530

    designed and developed login and register page

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
+import "../auth.form.scss"
+import {useNavigate, Link} from "react-router"
 
-export const Login = () => {
+const Login = () => {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+
   return (
     <main>
         <div className='form-container'>
             <h1>Login</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
-            <div className="input-group"></div>
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="text" id='email' name='email' placeholder='Enter email address'/>
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="text" id='password' name='password' placeholder='Enter password'/>
+            </div>
+
+            <button className='button primary-button'>Login</button>
 
             </form>
+
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
         </div>
     </main>
   )
 }
+
+export default Login
